8be81ee34e7da39fd43b894345c337e0e1f66243
pedrocb <pedrocb_c-s@hotmail.com>
2016-02-26 13:32:20 +0000

    Improved i3theme generation and changed theme colors

--- script.py
+++ script.py
@@ -3,34 +3,45 @@ import random
 import json
 import colorsys
 
-colorSaturation = 50
 colorHue = random.randint(0,360)
+testAngleBarColor = 276
+testAngleBarFontColor = 0
 
-mainColor = (colorHue,34,61)
-secondaryColorOne = (colorHue + (180 + 42)%360,70,30)
-secondaryColorTwo = (colorHue + (180 - 42)%360,70,30)
+mainColor = (colorHue,50,18)
+secondaryColorOne = ((colorHue + testAngleBarColor)%360,70,30)
+secondaryColorTwo = ((colorHue + testAngleBarFontColor)%360,70,30)
 complementaryColor = ((colorHue + 180)%360,70,30)
 
 def rgb_to_hexadecimal(color):
     return '%02x%02x%02x' % tuple([z * 255 for z in color])
 
+def hsv_to_rgb(color):
+    return colorsys.hsv_to_rgb(color[0]/360.0,color[1]/100.0,color[2]/100.0)
+
 def generate_wallpaper():
+    colorSaturation = 50
     os.system("python ~/wallpaper_changer/script.py " + str(colorHue) + " " + str(colorSaturation) + " ~/wallpaper_changer/1941277.png wallpaper.png")
     os.system("feh --bg-scale wallpaper.png")
-    os.system("rm wallpaper.png")
+    #os.system("rm wallpaper.png")
     return
 
 def generate_i3_theme():
+    barBackgroundColor = secondaryColorOne
+    barFontColor = (secondaryColorTwo[0],30,90)
+    barSeparatorColor = (secondaryColorTwo[0],70,90)
+    windowBorderColor = (mainColor[0],100,60)
+    focusedWorkspaceBackgroundColor = barBackgroundColor#(mainColor[0],50,50) #80 50
+    unfocusedWindowBorderColor = mainColor
     theme = "# vim: filetype=yaml\n---\nmeta:\n  description: AUTOMATICALLY GENERATED THEME\n"
     theme += "bar_colors:\n  background: '{0}'\n  separator: '{1}'\n  statusline: '{2}'\n  focused_workspace:\n    border: '{3}'\n    background: '{4}'\n    text: '{5}'\n\n".format('barbg','barseparator','barfont','barfont','focusedwsbg','barfont')
     theme += "window_colors:\n  focused:\n    border: windowbg\n    background: windowbg\n    text: barseparator\n  unfocused:\n    border: windowbg\n    background: unfocusedwindowbg\n    text: barbg\n\n"
     theme+= "colors:\n"
-    theme+= "  barbg: '{}'\n".format('#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(complementaryColor[0]/360.0,complementaryColor[1]/100.0,complementaryColor[2]/100.0) ]))
-    theme+= "  barfont: '{}'\n".format('#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,40/100.0,100/100.0) ]))
-    theme+= "  barseparator: '{}'\n".format('#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,100/100.0,100/100.0) ]))
-    theme+= "  windowbg: '{}'\n".format('#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,100/100.0,60/100.0) ]))
-    theme+= "  focusedwsbg: '{}'\n".format('#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,80/100.0,50/100.0) ]))
-    theme+= "  unfocusedwindowbg: '{}'\n".format('#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,100/100.0,0/100.0) ]))
+    theme+= "  barbg: '#{}'\n".format(rgb_to_hexadecimal(hsv_to_rgb(barBackgroundColor)))
+    theme+= "  barfont: '#{}'\n".format(rgb_to_hexadecimal(hsv_to_rgb(barFontColor)))
+    theme+= "  barseparator: '#{}'\n".format(rgb_to_hexadecimal(hsv_to_rgb(barSeparatorColor)))
+    theme+= "  windowbg: '#{}'\n".format(rgb_to_hexadecimal(hsv_to_rgb(windowBorderColor)))
+    theme+= "  focusedwsbg: '#{}'\n".format(rgb_to_hexadecimal(hsv_to_rgb(focusedWorkspaceBackgroundColor)))
+    theme+= "  unfocusedwindowbg: '#{}'\n".format(rgb_to_hexadecimal(hsv_to_rgb(unfocusedWindowBorderColor)))
     
     theme_file = open("theme.yaml","w")
     theme_file.write(theme)
@@ -51,7 +62,7 @@ def generate_rofi_theme():
     s20b100 = '#%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,20/100.0,100/100.0) ])
     s0b25 = '%02x%02x%02x'% tuple([z * 255 for z in colorsys.hsv_to_rgb(colorHue/360.0,0/100.0,25/100.0) ])
     color_window = "rofi.color-window: argb:ed{}, argb:ed{}, {}".format(windowBackgroundColor,windowBackgroundColor,s100b80)
-    color_normal = "rofi.color-normal: argb:ed{}, {}, argb:ed{}, #{}, {}".format(windowBackgroundColor,s100b80,windowBackgroundColor,highlightRowBackgroundColor,s100b70)
+    color_normal = "rofi.color-normal: argb:ed{}, {}, argb:ed{}, #{}, {}".format(windowBackgroundColor,s100b80,windowBackgroundColor,highlightRowBackgroundColor,s100b80)
     color_active = "rofi.color-active: argb:ed{}, {}, {}, {}, {}".format(windowBackgroundColor,s50b100,s0b20,s0b20,s20b100)
 
     os.system("sed -i '/color-window/ c {}' ~/.Xresources".format(color_window))
